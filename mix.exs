a9fab7f73178a1bdbe9326ec54d132a473022aac
TheFirstAvenger <mike@mikebinns.net>
2019-04-11 16:54:06 -0400

    Package info

--- mix.exs
+++ mix.exs
@@ -19,7 +19,10 @@ defmodule LogicSim.MixProject do
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      source_url: "https://github.com/TheFirstAvenger/logic_sim",
+      description: description()
     ]
   end
 
@@ -43,5 +46,16 @@ defmodule LogicSim.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/TheFirstAvenger/logic_sim"}
+    ]
+  end
+
+  defp description do
+    "Logical Circuit Simulator using GenServers and Message Passing"
   end
 end
