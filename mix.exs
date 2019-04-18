36e2f14492b1701dda7884c8e2090e41cb889729
TheFirstAvenger <mike@mikebinns.net>
2019-04-18 09:46:07 -0400

    v0.1.3

--- mix.exs
+++ mix.exs
@@ -17,7 +17,7 @@ defmodule LogicSim.MixProject do
         plt_file: {:no_warn, "logic_sim.plt"}
       ],
       app: :logic_sim,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
